2da39c4c3c5131f6bc187a9f8fd8cfde4353f652
David Scoffield <davidscoffield01@gmail.com>
2024-10-24 21:50:04 -0300

    Refactor OrderDAOHibernateJPA to add new methods for querying orders by delivery date, total, payment method, and emission/delivery date ranges

--- src/main/java/dao/impl/OrderDAOHibernateJPA.java
+++ src/main/java/dao/impl/OrderDAOHibernateJPA.java
@@ -28,11 +28,90 @@ public class OrderDAOHibernateJPA extends GenericDAOHibernateJPA<Order>
     }
   }
 
+  @Override
   public List<Order> getByClientId(Long clientId) {
     try (EntityManager em = HibernateUtil.getEntityManager()) {
       TypedQuery<Order> query = em.createQuery(
           "SELECT o FROM Order o WHERE o.client.id = :clientId", Order.class);
       query.setParameter("clientId", clientId);
+      return query.getResultList();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @Override
+  public List<Order> getByDeliveryDate(Date date) {
+    try (EntityManager em = HibernateUtil.getEntityManager()) {
+      TypedQuery<Order> query = em.createQuery(
+          "SELECT o FROM Order o WHERE o.deliveryDate = :date", Order.class);
+      query.setParameter("date", date);
+      return query.getResultList();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @Override
+  public List<Order> getHigherTotal(Float total) {
+    try (EntityManager em = HibernateUtil.getEntityManager()) {
+      TypedQuery<Order> query = em.createQuery(
+          "SELECT o FROM Order o WHERE o.total > :total", Order.class);
+      query.setParameter("total", total);
+      return query.getResultList();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @Override
+  public List<Order> getLowerTotal(Float total) {
+    try (EntityManager em = HibernateUtil.getEntityManager()) {
+      TypedQuery<Order> query = em.createQuery(
+          "SELECT o FROM Order o WHERE o.total < :total", Order.class);
+      query.setParameter("total", total);
+      return query.getResultList();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @Override
+  public List<Order> getByPaymentMethod(String paymentMethod) {
+    try (EntityManager em = HibernateUtil.getEntityManager()) {
+      TypedQuery<Order> query = em.createQuery(
+          "SELECT o FROM Order o WHERE o.paymentMethod = :paymentMethod",
+          Order.class);
+      query.setParameter("paymentMethod", paymentMethod);
+      return query.getResultList();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  @Override
+  public List<Order> getBetweenEmissionDates(Date startDate, Date endDate) {
+    try (EntityManager em = HibernateUtil.getEntityManager()) {
+      TypedQuery<Order> query = em.createQuery(
+          "SELECT o FROM Order o WHERE o.emissionDate BETWEEN :startDate AND :endDate",
+          Order.class);
+      query.setParameter("startDate", startDate);
+      query.setParameter("endDate", endDate);
+      return query.getResultList();
+    } catch (Exception e) {
+      return null;
+    }
+
+  }
+
+  @Override
+  public List<Order> getBetweenDeliveryDates(Date startDate, Date endDate) {
+    try (EntityManager em = HibernateUtil.getEntityManager()) {
+      TypedQuery<Order> query = em.createQuery(
+          "SELECT o FROM Order o WHERE o.deliveryDate BETWEEN :startDate AND :endDate",
+          Order.class);
+      query.setParameter("startDate", startDate);
+      query.setParameter("endDate", endDate);
       return query.getResultList();
     } catch (Exception e) {
       return null;
--- src/main/java/dao/interf/OrderDAO.java
+++ src/main/java/dao/interf/OrderDAO.java
@@ -6,7 +6,20 @@ import java.util.List;
 import model.Order;
 
 public interface OrderDAO extends GenericDAO<Order> {
-	public List<Order> getByEmissionDate(Date date);
-	
-	public List<Order> getByClientId(Long clientId);
+  public List<Order> getByEmissionDate(Date date);
+
+  public List<Order> getByDeliveryDate(Date date);
+
+  public List<Order> getByClientId(Long clientId);
+
+  public List<Order> getHigherTotal(Float total);
+
+  public List<Order> getLowerTotal(Float total);
+
+  public List<Order> getByPaymentMethod(String paymentMethod);
+
+  public List<Order> getBetweenEmissionDates(Date startDate, Date endDate);
+
+  public List<Order> getBetweenDeliveryDates(Date startDate, Date endDate);
+
 }
