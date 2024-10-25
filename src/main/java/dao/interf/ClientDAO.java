77a0f5533524f084b00f77d8b9c6b8d3ad4ba482
David Scoffield <davidscoffield01@gmail.com>
2024-10-25 17:37:03 -0300

    Refactor ClientDAOHibernateJPA to implement UserGenericDAO interface

--- src/main/java/dao/interf/ClientDAO.java
+++ src/main/java/dao/interf/ClientDAO.java
@@ -2,7 +2,6 @@ package dao.interf;
 
 import model.Client;
 
-public interface ClientDAO extends GenericDAO<Client> {
-  // ?? Como se relaciona para tener el comportameinto de UserDAO para ClientDAO
+public interface ClientDAO extends GenericDAO<Client>, UserGenericDAO {
 
 }
