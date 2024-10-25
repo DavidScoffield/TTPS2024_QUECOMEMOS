1f8af69b88b7a8ae2a1dbc177d8ab447747fd677
David Scoffield <davidscoffield01@gmail.com>
2024-10-25 09:25:43 -0300

    Refactor MenuDAOHibernateJPATest to import static assertTrue from org.junit.jupiter.api.Assertions

--- src/test/java/dao/impl/MenuDAOHibernateJPATest.java
+++ src/test/java/dao/impl/MenuDAOHibernateJPATest.java
@@ -2,10 +2,10 @@ package dao.impl;
 
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
