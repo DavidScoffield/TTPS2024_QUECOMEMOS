f198c152af77f2bbfad4693612345b659f0a38e0
David Scoffield <davidscoffield01@gmail.com>
2024-10-24 21:38:32 -0300

    Refactor OrderDAOHibernateJPA to use try-with-resources for EntityManager in getByEmissionDate and getByClientId methods

--- src/main/java/dao/impl/OrderDAOHibernateJPA.java
+++ src/main/java/dao/impl/OrderDAOHibernateJPA.java
@@ -10,36 +10,33 @@ import model.Order;
 import util.HibernateUtil;
 
 public class OrderDAOHibernateJPA extends GenericDAOHibernateJPA<Order>
-implements OrderDAO {
-	
-	public OrderDAOHibernateJPA() {
-	    super(Order.class);
-	  }
-	
-	  @Override
-	  public List<Order> getByEmissionDate(Date date) {
-	    EntityManager em = HibernateUtil.getEntityManager();
-	    try {
-	    	TypedQuery<Order> query = em.createQuery("SELECT o FROM Order o WHERE o.emissionDate = :date", Order.class);
-	        query.setParameter("date", date);
-	        return query.getResultList();
-	    } catch (Exception e) {
-	      return null;
-	    } finally {
-	      em.close();
-	    }
-	  }
-	  
-	  public List<Order> getByClientId(Long clientId) {
-		    EntityManager em = HibernateUtil.getEntityManager();
-		    try {
-		        TypedQuery<Order> query = em.createQuery("SELECT o FROM Order o WHERE o.client.id = :clientId", Order.class);
-		        query.setParameter("clientId", clientId);
-		        return query.getResultList();
-		    } catch (Exception e) {
-			      return null;
-			} finally {
-		        em.close();
-		    }
-		}
+    implements OrderDAO {
+
+  public OrderDAOHibernateJPA() {
+    super(Order.class);
+  }
+
+  @Override
+  public List<Order> getByEmissionDate(Date date) {
+    try (EntityManager em = HibernateUtil.getEntityManager()) {
+      TypedQuery<Order> query = em.createQuery(
+          "SELECT o FROM Order o WHERE o.emissionDate = :date", Order.class);
+      query.setParameter("date", date);
+      return query.getResultList();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  public List<Order> getByClientId(Long clientId) {
+    try (EntityManager em = HibernateUtil.getEntityManager()) {
+      TypedQuery<Order> query = em.createQuery(
+          "SELECT o FROM Order o WHERE o.client.id = :clientId", Order.class);
+      query.setParameter("clientId", clientId);
+      return query.getResultList();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
 }
