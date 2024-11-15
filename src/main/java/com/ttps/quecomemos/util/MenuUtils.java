9c639a2fe942a6c65957e6233aa5145ac2ea3133
David Scoffield <davidscoffield01@gmail.com>
2024-11-15 14:59:15 -0300

    Enhance validation in MenuUtils to check for null values in foodIds list

--- src/main/java/com/ttps/quecomemos/util/MenuUtils.java
+++ src/main/java/com/ttps/quecomemos/util/MenuUtils.java
@@ -1,26 +1,25 @@
 package com.ttps.quecomemos.util;
 
-
 import com.ttps.quecomemos.dto.MenuRegisterDTO;
 import com.ttps.quecomemos.errors.ValidationDataException;
 
-
 public class MenuUtils {
-	public static boolean isDataComplete(MenuRegisterDTO menuRegisterDTO)
-		      throws ValidationDataException {
-	    if (menuRegisterDTO.getName() == null) {
-	      throw new ValidationDataException("name", "Missing `name` field");
-	    }
+  public static boolean isDataComplete(MenuRegisterDTO menuRegisterDTO)
+      throws ValidationDataException {
+    if (menuRegisterDTO.getName() == null) {
+      throw new ValidationDataException("name", "Missing `name` field");
+    }
 
-	    if (menuRegisterDTO.getPrice() == null) {
-	      throw new ValidationDataException("price", "Missing `price` field");
-	    }
-	    
-	    if (menuRegisterDTO.getFoodsIds() == null || menuRegisterDTO.getFoodsIds().isEmpty()) {
-	        throw new ValidationDataException("foodIds", "The `foodIds` list is empty or missing.");
-	    }
+    if (menuRegisterDTO.getPrice() == null) {
+      throw new ValidationDataException("price", "Missing `price` field");
+    }
+    if (menuRegisterDTO.getFoodsIds() == null || menuRegisterDTO.getFoodsIds().isEmpty()
+        || menuRegisterDTO.getFoodsIds().contains(null)) {
+      throw new ValidationDataException("foodIds",
+          "The `foodIds` list is empty or missing. This field cannot contain null values");
+    }
 
-	    return true;
+    return true;
 
-	}
+  }
 }
