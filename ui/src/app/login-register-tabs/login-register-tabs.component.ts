b49e65e4be23a52d6f998e759ed0750686c16d76
David Scoffield <davidscoffield01@gmail.com>
2024-12-05 19:49:25 -0300

    Add error messages in login and register. Change to reactive forms

--- ui/src/app/login-register-tabs/login-register-tabs.component.ts
+++ ui/src/app/login-register-tabs/login-register-tabs.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core'
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
 import { provideIcons } from '@ng-icons/core'
 import { lucideChevronDown, lucideChevronUp } from '@ng-icons/lucide'
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm'
@@ -22,7 +29,7 @@ import {
 } from '@spartan-ng/ui-tabs-helm'
 
 @Component({
-  selector: 'spartan-tabs-preview',
+  selector: 'login-register-tabs',
   standalone: true,
   imports: [
     HlmTabsComponent,
@@ -43,6 +50,8 @@ import {
 
     BrnSelectImports,
     HlmSelectImports,
+
+    ReactiveFormsModule,
   ],
   providers: [provideIcons({ lucideChevronUp, lucideChevronDown })],
   host: {
@@ -50,4 +59,62 @@ import {
   },
   templateUrl: './login-register-tabs.component.html',
 })
-export class LoginRegisterTabs {}
+export class LoginRegisterTabs {
+  loginForm: FormGroup
+  registerForm: FormGroup
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      dni: [
+        '',
+        [Validators.required, Validators.minLength(7), Validators.maxLength(8)],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+
+    this.registerForm = this.fb.group(
+      {
+        dni: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(7),
+            Validators.maxLength(8),
+          ],
+        ],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        role: ['', [Validators.required]],
+      },
+      {
+        validators: [this.matchPasswords('password', 'confirmPassword')],
+      }
+    )
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log('Formulario de Login:', this.loginForm.value)
+    }
+  }
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      console.log('Formulario de Registro:', this.registerForm.value)
+    }
+  }
+
+  private matchPasswords(passwordKey: string, confirmPasswordKey: string) {
+    return (formGroup: AbstractControl) => {
+      const password = formGroup.get(passwordKey)?.value
+      const confirmPassword = formGroup.get(confirmPasswordKey)?.value
+
+      if (password !== confirmPassword) {
+        formGroup.get(confirmPasswordKey)?.setErrors({ passwordMismatch: true })
+      } else {
+        formGroup.get(confirmPasswordKey)?.setErrors(null)
+      }
+    }
+  }
+}
