5ff686ded33a5f105f411e58e599cbc1ccc4e642
David Scoffield <davidscoffield01@gmail.com>
2024-12-11 13:48:18 -0300

    Rename tokenInterceptorInterceptor to TokenInterceptor for consistency in naming

--- ui/src/app/interceptors/token-interceptor.interceptor.spec.ts
+++ ui/src/app/interceptors/token-interceptor.interceptor.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing'
 import { HttpInterceptorFn } from '@angular/common/http'
 
-import { tokenInterceptorInterceptor } from './token-interceptor.interceptor'
+import { TokenInterceptor } from './token-interceptor.interceptor'
 
 describe('tokenInterceptorInterceptor', () => {
   const interceptor: HttpInterceptorFn = (req, next) =>
-    TestBed.runInInjectionContext(() => tokenInterceptorInterceptor(req, next))
+    TestBed.runInInjectionContext(() => TokenInterceptor(req, next))
 
   beforeEach(() => {
     TestBed.configureTestingModule({})
