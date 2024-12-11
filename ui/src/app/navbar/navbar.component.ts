c7d8c9ad540355ca824e57c5dc2812fa1c03e75b
David Scoffield <davidscoffield01@gmail.com>
2024-12-11 16:51:27 -0300

    Implement dynamic navbar with user account menu and logout functionality

--- ui/src/app/navbar/navbar.component.ts
+++ ui/src/app/navbar/navbar.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core'
+import { Router, RouterLink } from '@angular/router'
+import { provideIcons } from '@ng-icons/core'
+import { lucideGlobe, lucideMicVocal } from '@ng-icons/lucide'
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm'
+import { BrnMenuTriggerDirective } from '@spartan-ng/ui-menu-brain'
+import {
+  HlmMenuBarComponent,
+  HlmMenuBarItemDirective,
+  HlmMenuComponent,
+  HlmMenuGroupComponent,
+  HlmMenuItemDirective,
+  HlmMenuSeparatorComponent,
+} from '@spartan-ng/ui-menu-helm'
+import { AuthService } from '../services/auth-service.service'
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
+  host: {
+    class: 'block',
+  },
+  imports: [
+    BrnMenuTriggerDirective,
+    HlmMenuComponent,
+    HlmMenuBarComponent,
+    HlmMenuItemDirective,
+    HlmMenuSeparatorComponent,
+    HlmMenuBarItemDirective,
+    HlmMenuGroupComponent,
+    HlmButtonDirective,
+    RouterLink,
+  ],
+  providers: [provideIcons({ lucideMicVocal, lucideGlobe })],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  get name(): string | undefined {
+    return this.authService.getData('user')?.name
+  }
+
+  get showNavbar(): boolean {
+    return this.router.url !== '/'
+  }
+
+  public logout(): void {
+    this.authService.logout()
+    this.router.navigate(['/'])
+  }
+}
