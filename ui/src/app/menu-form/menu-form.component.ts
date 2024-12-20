ee9ae5f4d5fc2a10f6cb507dd7aafbabf757e517
David Scoffield <davidscoffield01@gmail.com>
2024-12-20 17:46:25 -0300

    Refactor menu form to include separate selections for entrada, plato principal, bebida, and postre, and implement validation for required fields

--- ui/src/app/menu-form/menu-form.component.ts
+++ ui/src/app/menu-form/menu-form.component.ts
@@ -1,48 +1,61 @@
 import { CommonModule } from '@angular/common'
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 import {
+  AbstractControl,
   FormArray,
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
   Validators,
 } from '@angular/forms'
 import { ActivatedRoute, Router } from '@angular/router'
 import { MenuService } from '../services/menu.service'
 import { Food } from '../models/food.model'
-import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
-import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
-import { HlmCardContentDirective,
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm'
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm'
+import {
+  HlmCardContentDirective,
   HlmCardDescriptionDirective,
   HlmCardDirective,
   HlmCardFooterDirective,
   HlmCardHeaderDirective,
-  HlmCardTitleDirective } from '@spartan-ng/ui-card-helm';
-  import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
-  //import { BrnSelectImports } from '@spartan-ng/brain/select';
-  import { HlmSelectImports } from '@spartan-ng/ui-select-helm';
+  HlmCardTitleDirective,
+} from '@spartan-ng/ui-card-helm'
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm'
+//import { BrnSelectImports } from '@spartan-ng/brain/select';
+import { HlmSelectImports } from '@spartan-ng/ui-select-helm'
 import { FoodService } from '../services/food.service'
 import { Menu } from '../models/menu.model'
 
 @Component({
   selector: 'app-menu-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule,  HlmCardContentDirective, HlmButtonDirective,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HlmCardContentDirective,
+    HlmButtonDirective,
     HlmCardDescriptionDirective,
     HlmCardDirective,
     HlmCardFooterDirective,
     HlmCardHeaderDirective,
-    HlmCardTitleDirective, HlmInputDirective, HlmLabelDirective, HlmSelectImports],
+    HlmCardTitleDirective,
+    HlmInputDirective,
+    HlmLabelDirective,
+    HlmSelectImports,
+  ],
   templateUrl: './menu-form.component.html',
   styleUrls: ['./menu-form.component.css'],
 })
 export class MenuFormComponent implements OnInit {
-  @Input() menuId?: number; // Recibe el ID del menú en caso de edición
-  @Output() save = new EventEmitter<Menu>();
-  @Output() closeForm = new EventEmitter(); 
-  foodsLoaded:Food[] = []
-  menuForm: FormGroup;
-  isEditMode = false;
+  @Input() menuId?: number // Recibe el ID del menú en caso de edición
+  @Output() save = new EventEmitter<Menu>()
+  @Output() closeForm = new EventEmitter()
+  foodsLoaded: Food[] = []
+  foodsByType: { [key: string]: Food[] } = {}
+  menuForm: FormGroup
+  isEditMode = false
 
   constructor(
     private fb: FormBuilder,
@@ -55,66 +68,98 @@ export class MenuFormComponent implements OnInit {
       name: ['', Validators.required],
       picture: [''],
       price: ['', [Validators.required, Validators.min(0)]],
-      foods: this.fb.array([]),
-    });
+      entrada: ['', Validators.required],
+      platoPrincipal: ['', Validators.required],
+      bebida: ['', Validators.required],
+      postre: ['', Validators.required],
+    })
   }
 
   ngOnInit(): void {
     if (this.menuId) {
-      this.isEditMode = true;
-      
+      this.isEditMode = true
+
       // Suscribirse al Observable de getMenu()
-      this.menuService.getMenu(this.menuId).subscribe(menu => {
+      this.menuService.getMenu(this.menuId).subscribe((menu) => {
         // Una vez que recibimos el objeto 'menu', podemos acceder a sus propiedades
-        this.menuForm.patchValue(menu);
-        menu.foods.forEach((food) => this.addFood(food.id));
-      });
+        this.menuForm.patchValue(menu)
+        menu.foods.forEach((food) => this.addFood(food.id))
+      })
     }
-  
+
     // Cargar los alimentos independientemente
-    this.foodService.getFoods().subscribe((foods)=>{
-      this.foodsLoaded = foods;
-    });
+    this.foodService.getFoods().subscribe((foods) => {
+      this.foodsLoaded = foods
+
+      // Actualizar foodsByType agrupando los alimentos por tipo
+      this.foodsByType = this.groupFoodsByType(foods)
+      console.log('foodsByType', this.foodsByType)
+    })
+  }
+
+  // Función auxiliar para agrupar alimentos por tipo
+  private groupFoodsByType(foods: Food[]): { [key: string]: Food[] } {
+    return foods.reduce((acc, food) => {
+      if (!acc[food.type]) {
+        acc[food.type] = []
+      }
+      acc[food.type].push(food)
+      return acc
+    }, {} as { [key: string]: Food[] })
   }
 
   get foods() {
-    return this.menuForm.get('foods') as FormArray<FormGroup>;
+    return this.menuForm.get('foods') as FormArray<FormGroup>
   }
 
   addFood(foodId: number | void): void {
     const foodForm = this.fb.group({
       food: [Number(foodId) || '', Validators.required], // Guardamos solo el ID o vacío si no se pasa un valor
-    });
-    this.foods.push(foodForm);
+    })
+    this.foods.push(foodForm)
   }
 
   removeFood(index: number): void {
-    this.foods.removeAt(index);
+    this.foods.removeAt(index)
   }
 
   onSubmit(): void {
     if (this.menuForm.valid) {
-      const foodIds = this.menuForm.value.foods.map((food: { food: any }) => Number(food.food)); // Extraemos los IDs seleccionados
-  
+      // const foodIds = this.menuForm.value.foods.map((food: { food: any }) =>
+      //   Number(food.food)
+      // ) // Extraemos los IDs seleccionados
+
+      const foodIds = [
+        this.menuForm.value.entrada,
+        this.menuForm.value.platoPrincipal,
+        this.menuForm.value.bebida,
+        this.menuForm.value.postre,
+      ].filter((food) => Number(food) > 0)
+
+      if (foodIds.length >= 2) {
+        alert('Debes seleccionar al menos 2 comidas')
+        return
+      }
+
       const menuData = {
         ...this.menuForm.value,
         foods: [], // Vacío porque no necesitamos enviar objetos completos
         foodsIds: foodIds, // Enviamos los IDs de las comidas seleccionadas
-      };
-  
+      }
+
       if (this.isEditMode && this.menuId) {
         this.menuService.updateMenu(this.menuId, menuData).subscribe((menu) => {
-          this.save.emit(menu); // Notifica que el formulario debe cerrarse
-        });
+          this.save.emit(menu) // Notifica que el formulario debe cerrarse
+        })
       } else {
         this.menuService.addMenu(menuData).subscribe((menu) => {
-          this.save.emit(menu); // Notifica que el formulario debe cerrarse
-        });
+          this.save.emit(menu) // Notifica que el formulario debe cerrarse
+        })
       }
     }
   }
 
   onCancel(): void {
-    this.closeForm.emit(); // Emite el evento para cerrar el formulario sin guardar
+    this.closeForm.emit() // Emite el evento para cerrar el formulario sin guardar
   }
 }
